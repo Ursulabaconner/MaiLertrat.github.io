1f4347aead4891df008a87d2c19e32622d469ff0
Maika Baconner <87841333+Ursulabaconner@users.noreply.github.com>
2025-01-18 09:28:31 +0700

    uplond my profile

--- README.md
+++
@@ -1,1 +0,0 @@
-# MaiLertrat.github.io
---
+++ lang_en.php
@@ -0,0 +1,74 @@
+<?php
+return [
+    //top 
+    'welcome' => 'Mai Lertrat',
+
+    //menu header
+    'home' => 'Home',
+    'about' => 'About',
+    'quality' => 'Quality',
+    'skills' => 'Skills',
+    'service' => 'Service',
+    'portfolio' => 'Portfolio',
+    'contact' => 'Contact',
+    
+    //about me
+    'about_head' => 'About Me',
+    'detail' => 'I am always eager to learn new technologies and improve my skills, with the aim of delivering high-quality work. My goal is to contribute to a forward-thinking company where I can grow professionally while making an impact.',
+    'name' => 'Songkran Lertrat',
+    'degree' => "Bachelor's degree",
+    'address' => '15 Street 15, Chiang Mai, Thailand',
+    'birthday' => '13 April 2001',
+    'experience' => 'Years',
+    'freelance' => 'Unavailable',
+
+    //Education & Expericence
+    //Education
+    'education_expericence_head' => 'Education & Expericence',
+    'education_head' => 'My Education',
+    'gradelevel1' => 'Vocational Certificate',
+    'where1' => 'Metro Technology College',
+    'year1' => "2017 - 2020",
+    'education_detail1' => 'Graduated from the field of industrial electronics. Did an internship at a computer repair shop for 3 months. The project I worked on was firefighting. Won a prize in a private vocational skills competition. Department of Electronics Engineering, Digital Skills, First Place Winner',
+    'gradelevel2' => "High Vocational Certificate",
+    'where2' => 'Metro Technology College',
+    'year2' => "2020 - 2022",
+    'education_detail2' => 'Graduated from Information Technology field The project that I m doing is a game from RPG Maker MV. The game name is CMT. Its an RPG game, collecting levels, fighting monsters and has a story to follow. Won a prize in the private vocational skills competition. Subject type: Information and Communication Technology Team computer programming skills First place winner',
+    'gradelevel3' => "Bachelor's degree",
+    'where3' => 'The Far Eastern University',
+    'year3' => "2022 - 2023",
+    'education_detail3' => 'Graduated from the field of digital technology Good Student Award for 2024, with the project being a website selling Cosplay Online products.',
+    //Experience
+    'experience_head' => 'My Expericence',
+    'experience1' => 'IT Admin',
+    'company1' => 'ORNATE SDO',
+    'year4' => "2024 - Now",
+    'experience_detail1' => 'IT administrators manage and maintain computer networks within an organization at ORNATE SDO. in full-stack website development My expertise lies in PHP, SQL and modern web technologies. I enjoy solving complex technical challenges and have a passion for developing efficient, scalable solutions.',
+
+    //my service
+    'service_head' => 'My Service',
+    'service1' => 'Web Development',
+    'service_detail1' => 'With over 2 years of experience in web development, I specialize in building responsive, user-friendly websites using modern technologies such as HTML, CSS, JavaScript, and PHP. My focus is on delivering efficient solutions tailored to client needs.',
+    'service2' => 'E-commerce Solutions',
+    'service_detail2' => 'I offer tailored e-commerce solutions with over 2 years of experience in designing and developing online stores. Whether you re looking to build a new online shop or enhance your current platform, I provide solutions that improve user experience and increase sales.',
+    'service3' => 'Data Analyst',
+    'service_detail3' => 'As an IT Admin at ORNATE SDO, I have extensive experience in analyzing data to drive insights and improve decision-making. I specialize in data visualization, report generation, and working with large datasets to identify trends and opportunities.',
+    'service4' => 'Database Administrator(DBA)',
+    'service_detail4' => 'Expert in database management, ensuring optimal performance, security, and integrity of data systems. My experience includes database design, maintenance, and performance tuning for robust and scalable solutions.',
+    'service5' => 'Software Development Consulting',
+    'service_detail5' => 'I provide expert software development consulting with 2 years of experience in web development. I assist businesses in identifying the best tools and strategies for their projects, from planning to execution, ensuring success in every stage of the development cycle.',
+    'service6' => 'Software Tester',
+    'service_detail6' => 'With a focus on quality assurance, I have experience in software testing, ensuring applications are reliable and bug-free. I conduct thorough testing processes, including functional, regression, and performance testing, to ensure smooth user experiences.',
+    
+    //Portfolio
+    'portfolio_head' => 'My Portfolio',
+
+    //Contact Me
+    'contact_head' => 'Contact Me',
+    'name_sead' => 'Your Name',
+    'email_sead' => 'Your Email',
+    'subject_sead' => 'Subject',
+    'message_sead' => 'Message',
+
+];
+?>
